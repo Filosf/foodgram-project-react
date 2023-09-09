46c09ca33ef2187ae3ab6890c42f2b3f8507ec23
Ìêàêñè Ìàòâååâ <Filosf5@yandex.ru>
2023-09-09 18:24:49 +0300

    Update IngredientViewSet 2.0

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -90,8 +90,9 @@ class IngredientViwsSet(ModelViewSet):
 
     def get_queryset(self):
         queryset = Ingredient.objects.all()
-        queryset = queryset.annotate(lower_name=functions.Lower("name"))
-        queryset = queryset.order_by("lower_name")
+        queryset.annotate(
+            lower_name=functions.Lower("name")
+        ).order_by("lower_name")
         return queryset
 
 
